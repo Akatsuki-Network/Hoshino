7320acfba6a0abcf3c952f5c2c932b3951469ea3
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-22 19:37:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 from sys import exit
 
-API_ID = os.getenv("API_ID", 28122413) or (print("API_ID is not defined.") or exit(1))
+API_ID = os.getenv("API_ID", "28122413") or (print("API_ID is not defined.") or exit(1))
 API_HASH = os.getenv("API_HASH", "750432c8e1b221f91fd2c93a92710093") or (print("API_HASH is not defined.") or exit(1))
 BOT_TOKEN = os.getenv("BOT_TOKEN", "7680292640:AAFMnLNbZcmkkEILKIw5MpgrtqGDLhDG5hs") or (print("BOT_TOKEN is not defined.") or exit(1))
 #SUPPORT_CHAT = os.getenv("SUPPORT_CHAT") or (logger.error("SUPPORT_CHAT is not defined.") or exit(1))
