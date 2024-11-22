7987b7812073a20479a853c7dc742d53a743c708
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-22 20:16:49 +0530

    Update __main__.py

--- Hoshino/__main__.py
+++ Hoshino/__main__.py
@@ -1,10 +1,14 @@
 from Hoshino import ptbhoshi, pyrohoshi
+import asyncio
 
-async def main():    
-   pyrohoshi().run()   
-    
+async def main():
+    await pyrohoshi().start()
+
 if __name__ == "__main__":
-   ptbhoshi.run_polling()  
-   pyrohoshi().loop.run_until_complete(main())
-   
-    
+    loop = asyncio.get_event_loop()
+
+    ptb_task = loop.create_task(ptbhoshi.run_polling())
+
+    loop.run_until_complete(main())
+
+    loop.run_until_complete(ptb_task)
