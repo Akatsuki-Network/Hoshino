0f38f6f47ff9bb113dfb2c63d73355a150c082c0
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-22 19:50:47 +0530

    Update test.py

--- Hoshino/plugins/test.py
+++ Hoshino/plugins/test.py
@@ -3,10 +3,16 @@ from Hoshino.helpers.pdecorators import command
 
 
 @Client.on_message(filters.command("start"))
-async def start(client:Client, message: t.Message):
+async def start(client, message):
     await message.reply_text("Hi Pyro")
 
 
 @command("pstart")
 async def pstart(update, context):
     await update.message.reply_text("Hi PTB")
+
+
+__MODULE__ = "START"
+
+__HELP__ = "HI"
+
