9d0346894f76daaf7d583dd47a88a0e772336498
Siya Lokhande <142929991+siyu-xd@users.noreply.github.com>
2024-11-22 19:16:20 +0530

    Create test.py

---
+++ Hoshino/plugins/test.py
@@ -0,0 +1,12 @@
+from pyrogram import Client, filters, types as t
+from Hoshino.helpers.decorators import command 
+
+
+@Client.on_message(filters.command("start"))
+async def start(client:Client, message: t.Message):
+    await message.reply_text("Hi Pyro")
+
+
+@command("pstart")
+async def pstart(update, context):
+    await update.message.reply_text("Hi PTB")
