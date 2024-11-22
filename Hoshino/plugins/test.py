f83bfaa3ec1c24a047bf96dd9314a1871b932652
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-22 20:03:32 +0530

    Update test.py

--- Hoshino/plugins/test.py
+++ Hoshino/plugins/test.py
@@ -1,10 +1,14 @@
-from pyrogram import Client, filters, types as t
-from Hoshino.helpers.pdecorators import command 
+from Hoshino.helpers.pdecorators import command
+from pyrogram import Client, filters, types as t, enums
 
-
-@Client.on_message(filters.command("start"))
-async def start(client, message):
-    await message.reply_text("Hi Pyro")
+@Client.on_message(filters.command(["start", "help", "repo", "source"]))
+async def start(_: Client, m: t.Message):
+    await m.reply_text(
+        text="under maintenance",
+        reply_markup=t.InlineKeyboardMarkup(
+            [[t.InlineKeyboardButton(text="Source", url="https://github.com/Akatsuki-Network/Hoshino")]]
+        ),
+    )
 
 
 @command("pstart")
