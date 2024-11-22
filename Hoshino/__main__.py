1964ba4d54b4b62d4f6fd1b946ec5408cfc85ee0
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-22 20:40:32 +0530

    Update __main__.py

--- Hoshino/__main__.py
+++ Hoshino/__main__.py
@@ -1,9 +1,9 @@
 from Hoshino import ptbhoshi, pyrohoshi
 
-async def main():
-    await ptbhoshi.initialize()
+async def main():    
     await pyrohoshi.start()
 
 if __name__ == "__main__":
     import asyncio
+    ptbhoshi.run_polling()
     asyncio.run(main())
