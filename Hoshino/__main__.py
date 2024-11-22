68754a75e05100da4533d966dff991bcc0eb21df
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-22 20:28:16 +0530

    Update __main__.py

--- Hoshino/__main__.py
+++ Hoshino/__main__.py
@@ -1,9 +1,14 @@
 from Hoshino import ptbhoshi, pyrohoshi
+import pyrogram
 
 def main():
     ptbhoshi.run_polling()
-    
+
+async def run_clients():
+      await pyrohoshi.start()
+      await pyrogram.idle()
+             
 if __name__ == "__main__":
     main()
-    pyrohoshi.start()
+    pyrohoshi.loop.run_until_complete(run_clients())
     
