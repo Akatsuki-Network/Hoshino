b01622a907d5af26f6513ef701fb874729276439
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-23 11:51:52 +0530

    Update test.py

--- Hoshino/plugins/test.py
+++ Hoshino/plugins/test.py
@@ -3,20 +3,21 @@ from pyrogram import Client, filters, types as t, enums
 
 @Client.on_message(filters.command(["start", "help", "repo", "source"]))
 async def start(_: Client, m: t.Message):
-    await m.reply_text(
-        text="under maintenance",
-        reply_markup=t.InlineKeyboardMarkup(
-            [[t.InlineKeyboardButton(text="Source", url="https://github.com/Akatsuki-Network/Hoshino")]]
-        ),
-    )
-
+    if len(m.text.split()) > 1:
+        name = m.text.split(None, 1)[1]
+        if name.startswith("help"):
+            await bot.show_help_menu(m.chat.id, page=1)
+    else:
+        await m.reply_text(
+            text="under maintenance",
+            reply_markup=t.InlineKeyboardMarkup(
+                [[t.InlineKeyboardButton(text="Source", url="https://github.com/Akatsuki-Network/Hoshino")]]
+            ),
+        )
 
 @command("pstart")
 async def pstart(update, context):
     await update.message.reply_text("Hi PTB")
 
-
 __MODULE__ = "START"
-
 __HELP__ = "HI"
-
