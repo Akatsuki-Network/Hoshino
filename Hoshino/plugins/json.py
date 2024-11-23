19d624283ed72e06fd94cefb8ec5bdcf18f7731f
Vishal-Borase <vishalborse2885@gmail.com>
2024-11-23 09:43:29 +0530

    Update json.py

--- Hoshino/plugins/json.py
+++ Hoshino/plugins/json.py
@@ -1,6 +1,5 @@
 from pyrogram import filters, Client
 from pyrogram.types import Message
-import json
 from pyrogram import enums
 
 __MODULE__ = "json"
@@ -9,5 +8,4 @@ __HELP__ = "Send the JSON representation of a message or the replied-to message.
 @Client.on_message(filters.command("json"))
 async def json_command(client, message: Message):
     msg = message.reply_to_message if message.reply_to_message else message
-    json_data = json.dumps(msg, indent=4, default=str)
-    await message.reply_text(f"```{json_data}```", parse_mode=enums.ParseMode.MARKDOWN)
+    await message.reply_text(f"`{msg}`")
