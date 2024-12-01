5daa49404c7e8a47e9adf3c4ac87f8fb5b8cd8d1
Vishal-Borase <vishalborse2885@gmail.com>
2024-12-01 12:09:49 +0530

    Update __main__.py

--- Hoshino/__main__.py
+++ Hoshino/__main__.py
@@ -1,10 +1,19 @@
-from Hoshino import ptbhoshi, pyrohoshi, setup_telegram_logging, hoshi
+from Hoshino import ptbhoshi, pyrohoshi, setup_telegram_logging, hoshi, capture_err
+from config import CHAT_ID
+
+@capture_err
+async def sample_command(pyrohoshi, message):
+    raise ValueError("Test error handling")
 
 def main() -> None:
     ptbhoshi.run_polling(drop_pending_updates=True)
 
 if __name__ == "__main__":
-    pyrohoshi.start()  
-    setup_telegram_logging()  # Set up Telegram log handler after Pyro client starts
-    hoshi.info("Pyrogram client started successfully.")
-    main()
+    try:
+        pyrohoshi.start()  
+        setup_telegram_logging()
+        hoshi.info("Pyrogram client started successfully.")
+        main()
+    except Exception as e:
+        hoshi.error(f"Failed to start bot due to error: {e}")
+        pyrohoshi.send_message(CHAT_ID, f"**Critical Error in Bot Startup**: {e}")
