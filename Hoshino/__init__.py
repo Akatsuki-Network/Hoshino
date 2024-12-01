f6ae41efb8090fc446e1405ff6b258d55bc3d954
Vishal-Borase <vishalborse2885@gmail.com>
2024-12-01 12:07:25 +0530

    Update __init__.py

--- Hoshino/__init__.py
+++ Hoshino/__init__.py
@@ -7,6 +7,7 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from Helpo import Helpo
 from config import BOT_TOKEN, API_ID, API_HASH, CHAT_ID
 import asyncio
+from functools import wraps
 
 logging.basicConfig(
     format="%(asctime)s - [HOSHINO] - %(levelname)s - %(name)s - %(message)s",
@@ -43,7 +44,6 @@ pagination = Helpo(
     texts=custom_texts,
 )
 
-
 class TelegramLogHandler(logging.Handler):
     def __init__(self, chat_id, client: Client):
         super().__init__()
@@ -67,7 +67,6 @@ class TelegramLogHandler(logging.Handler):
         except Exception as e:
             hoshi.error(f"Failed to send log to Telegram: {e}")
 
-
 def setup_telegram_logging():
     telegram_handler = TelegramLogHandler(chat_id=CHAT_ID, client=pyrohoshi)
     telegram_handler.setLevel(logging.WARNING)
@@ -75,15 +74,58 @@ def setup_telegram_logging():
     telegram_handler.setFormatter(formatter)
     hoshi.addHandler(telegram_handler)
 
-
 def handle_exception(exc_type, exc_value, exc_traceback):
     if issubclass(exc_type, KeyboardInterrupt):
         sys.__excepthook__(exc_type, exc_value, exc_traceback)
         return
     trace = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
     hoshi.critical(f"Unhandled exception:\n{trace}")
-
+    error_feedback = split_limits(trace)
+    for x in error_feedback:
+        pyrohoshi.send_message(CHAT_ID, f"**Unhandled Exception**: `{x}`")
 
 sys.excepthook = handle_exception
 
 hoshi.info("Application initialized. Logging is active for warnings and above.")
+
+
+def capture_err(func):
+    @wraps(func)
+    async def capture(client, message, *args, **kwargs):
+        try:
+            return await func(client, message, *args, **kwargs)
+        except Exception as err:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            errors = traceback.format_exception(
+                etype=exc_type, value=exc_obj, tb=exc_tb,
+            )
+            error_feedback = split_limits(
+                '**ERROR** | `{}` | `{}`\n\n```{}```\n\n```{}```\n'.format(
+                    0 if not message.from_user else message.from_user.id,
+                    0 if not message.chat else message.chat.id,
+                    message.text or message.caption,
+                    ''.join(errors),
+                ),
+            )
+            for x in error_feedback:
+                await pyrohoshi.send_message(
+                    CHAT_ID,
+                    x
+                )
+            raise err
+    return capture
+
+def split_limits(text):
+    if len(text) < 2048:
+        return [text]
+    lines = text.splitlines(True)
+    small_msg = ''
+    result = []
+    for line in lines:
+        if len(small_msg) + len(line) < 2048:
+            small_msg += line
+        else:
+            result.append(small_msg)
+            small_msg = line
+    result.append(small_msg)
+    return result
